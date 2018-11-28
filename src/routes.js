126877ba45d56688e832c6f300e60c7171896e40
Renan Machado <renan.mav@hotmail.com>
2018-11-28 08:53:43 -0300

    Require-dir

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,6 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
-
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 

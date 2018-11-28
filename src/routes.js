4bc5270a444810a865f4c1ca44c8410af02d4e00
Renan Machado <renan.mav@hotmail.com>
2018-11-28 08:53:47 -0300

    Revert "Require-dir"
    This reverts commit 126877ba45d56688e832c6f300e60c7171896e40.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 

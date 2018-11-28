a55d2209c93e67c5fdb2f4cd755f95e5b537261b
Renan Machado <renan.mav@hotmail.com>
2018-11-28 13:23:07 -0300

    Email de solicitação de compra

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '386d92fe073d48',
+    pass: '9129d782fdcc3d'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -7,16 +7,23 @@ const authMiddleware = require('./app/middlewares/auth')
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const AdController = require('./app/controllers/AdController')
+const PurchaseController = require('./app/controllers/PurchaseController')
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware)
 
+// ADS
+
 routes.get('/ads', AdController.index)
 routes.get('/ads/:id', AdController.show)
 routes.post('/ads', AdController.store)
 routes.put('/ads/:id', AdController.update)
 routes.delete('/ads/:id', AdController.destroy)
+
+// PURCHASE
+
+routes.post('/purchases', PurchaseController.store)
 
 module.exports = routes

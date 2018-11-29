b81aaebe7486ba36856d718b03f81420216c067d
Renan Machado <renan.mav@hotmail.com>
2018-11-29 15:19:42 -0300

    Sentry

--- src/app/services/Queue.js
+++ src/app/services/Queue.js
@@ -1,9 +1,12 @@
 const kue = require('kue')
 const redisConfig = require('../../config/redis')
 const PurchaseMail = require('../jobs/PurchaseMail')
+const Sentry = require('@sentry/node')
 
 const Queue = kue.createQueue({ redis: redisConfig })
 
 Queue.process(PurchaseMail.key, PurchaseMail.handle)
+
+Queue.on('error', Sentry.captureException)
 
 module.exports = Queue
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: 'https://c1fb3b2fab164d93908b9566d438603f@sentry.io/1333741'
+}

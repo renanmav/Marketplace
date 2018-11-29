3b8ed28053f4a01ac9996a367351d254c39233e9
Renan Machado <renan.mav@hotmail.com>
2018-11-29 15:40:20 -0300

    Variáveis ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: 'GoNode',
+  secret: process.env.APP_SECRET,
   ttl: 86400
 }
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '386d92fe073d48',
-    pass: '9129d782fdcc3d'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
